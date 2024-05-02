0a4885956e4e8a6c73cc9663fb1db44f96a10f83
unknown <yency072005@gmail.com>
2024-05-02 11:41:47 -0500

    php

---
+++ PHP/includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introducción a PHP - SENA</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+    <div class="contenido">
+        <h1>Desarrollo Web Completo</h1>
+        <a href="http://www.sena.edu.co" target="_blank">Servicio Nacional de Aprendizaje</a>
+        <p>Introducción a PHP</p>
+        <p class="hashtag">#Desarrollo Web con PHP</p>
+
+        <div class="resultado">
+
+        </div>
+
+    </div>
+    
+
+
+</body>
+</html>
