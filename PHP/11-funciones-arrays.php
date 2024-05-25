b0436c159427b2c67d8755b73f62c5a83fb43b83
YencyKarinaRodriguez <129075276+YencyKarinaRodriguez@users.noreply.github.com>
2024-05-25 17:27:18 -0500

    ...

--- PHP/11-funciones-arrays.php
+++ PHP/11-funciones-arrays.php
@@ -4,6 +4,33 @@
 $carrito = ['Tablet', 'Computador','Impresora', 'Teclado'];
 var_dump(in_array('Tablet', $carrito));
 
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' =>100,
+    'edad' => 34
+];
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+// Orderar Arreglos asociativos por el valor de las propiedades
+asort($cliente);
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+// Orderar Arreglos asociativos por las Llaves Ascendente
+ksort($cliente);
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+// Onderar Arreglos asociativos por las llaves - Descendente
+krsort($cliente);
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
 //Ordenar arreglos
 $aleatorios = [4,6,2,9,6,4,3,9,7, "arbol", "armonia"];
 rsort($aleatorios);
---
+++ PHP/12-condicionales.php
@@ -0,0 +1,58 @@
+<?php include 'includes/encabezado.php';
+
+$autenticado = true;
+$admin = false;
+
+if($autenticado && $admin ) {
+    echo "Usuario autenticado correctamente";
+} else {
+    echo "Usuario no autenticado, inicia sesión";
+}
+
+// If anidados...
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'información' => [
+        'tipo' => 'Premiun'
+    ]
+];
+
+echo "<br>";
+if ( !empty($cliente) ) {
+    echo "El Arreglo de cliente no esta vacio";
+    
+    if ($cliente ['saldo'] > 0) {
+        echo "El Cliente tiene saldo disponible";
+    } else {
+        echo "No hay saldo";
+    }
+}
+
+// else if
+if($cliente['saldo'] > 0) {
+    echo "El cliente tiene saldo";
+} else if ($cliente['informacion']['tipo'] === 'Premiun') {
+    echo "El cliente es premium";
+} else {
+    echo "No hay cliente definido o no tiene saldo o no es premium";
+}
+
+// switch
+$tecnologia = 'PHP';
+switch ($tecnologia) {
+    case 'PHP':
+        echo "PHP, un excenlente lenguaje!";
+        break;
+    case 'JavaScript':
+        echo "Genial,el lenguaje de la web";
+        break;
+    case 'HTML':
+        echo "Emmm";
+        break;
+    default:
+    echo "Algun lenguaje que no se cual es";
+    break;
+}
+
+include 'includes/pie de página.php';
